64c5898852b0503cda843ec75f75d0e06f08217c
Pierre Jaffuer <p.jaffuer@free.fr>
2023-03-24 14:14:52 +0400

    Test sprite loading

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,9 @@
 *
 ********************************************************************************************/
 
-#include "raylib-cpp.hpp"
+#include "raylib-cpp.hpp"    // Raylib
+#include "external/raygui/raygui.h" // GUI lib for debug
 
-//-------------------------------------------------------
-//  RAYGUI
-//-------------------------------------------------------
-#define RAYGUI_IMPLEMENTATION // THIS FIRST
-#include "external/raygui.h"
-//-------------------------------------------------------
-
-#include "Sprite.h"
-#include <memory>
 
 #if defined(PLATFORM_WEB)
     #include <emscripten/emscripten.h>
@@ -37,25 +29,6 @@ int main(void)
     raylib::Window window(screenWidth, screenHeight, "Test sprite animations");
     window.SetTargetFPS(60);
 
-    std::shared_ptr<raylib::Texture2D> texture = std::make_shared<raylib::Texture2D>("./data/tux.png");
-
-    AnimatedSprite player;
-    player.position = raylib::Vector2(0,0);
-
-    auto animations = LoadAnimationFrames("./data/tux.txt");
-
-    for(auto& pair : animations){
-        SpriteAnimation anim;
-        anim.frames = pair.second;
-        anim.spriteSheet = texture;
-
-        anim.speed = 8;
-
-        player.SetAnimation(pair.first, anim);
-    }
-
-    player.PlayAnimation("run");
-
     raylib::Camera2D cam;
     cam.offset = {screenWidth/2, screenHeight/2};
     cam.rotation = 0;
@@ -67,21 +40,10 @@ int main(void)
     {
         window.BeginDrawing();
         window.ClearBackground(WHITE);
-        float y = 0;
-        if(GuiButton({0, y,128,64}, "idle")){
-            player.PlayAnimation("idle");
-        }
-        y += 64+8;
-        if(GuiButton({0, y,128,64}, "walk")){
-            player.PlayAnimation("walk");
-        }
-        y += 64+8;
-        if(GuiButton({0, y,128,64}, "run")){
-            player.PlayAnimation("run");
-        }
+
+            // Cam view
             cam.BeginMode();
 
-                player.Draw(window.GetFrameTime(), true);
 
             cam.EndMode();
         window.EndDrawing();
